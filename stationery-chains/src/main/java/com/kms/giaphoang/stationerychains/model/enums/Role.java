99adc26601ad5e3c5ad4d882206962d2757d35b8
giaphoang <hhgiap241>
2022-08-15 15:16:11 +0700

    Config security for keycloak

---
+++ stationery-chains/src/main/java/com/kms/giaphoang/stationerychains/model/enums/Role.java
@@ -0,0 +1,12 @@
+package com.kms.giaphoang.stationerychains.model.enums;
+
+/**
+ * @author : giaphoang
+ * @mailto : hoanghuugiap241@gmail.com
+ * @created : 8/12/2022, Friday
+ * @project: stationery
+ **/
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
